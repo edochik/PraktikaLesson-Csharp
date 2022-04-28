00ebb0ff6a1760a4ef7ef70419be79d338ea19fb
edochik <edoch@yandex.ru>
2022-04-28 23:18:39 +0300

    update information

---
+++ Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿//4.	Написать программу замену элементов массива на противоположные 
+
+void PrintNew(int[] array)
+{
+    int lenght = array.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        array[i] = new Random().Next(99, 1000);
+        Console.Write($" {array[i]}");
+    }
+}
+
+void PrinNegative(int[] array) // 4. Написать программу замену элементов массива на противоположные
+{
+    int lenght = array.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        array[i] = -1 * array[i];
+        Console.Write(array[i]);
+    }
+}
+
+int[] array = new int[8];
+
+PrintNew(array);
+Console.WriteLine();
+PrinNegative(array);
+/*
+PrintNew(array);
+
+int a = 1;
+
+int b = a * (-1);
+Console.WriteLine(a + " " + b);*/
+
