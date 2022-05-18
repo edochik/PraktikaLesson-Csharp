e91dfd99812ab0af6ca22d4d91f753fd5b45992b
edochik <edoch@yandex.ru>
2022-05-18 21:20:38 +0300

    add 7 homework

--- trening/Program.cs
+++ trening/Program.cs
@@ -48,3 +48,104 @@ Console.WriteLine(EvenNum(test));
 
 
 */
+
+
+/*
+1. Спирально заполнить двумерный массив:
+  1  2  3  4
+ 12 13 14  5
+ 11 16 15  6
+ 10  9  8  7 
+*/
+//1. Как печатать массив по порядку? (нашел)
+//2. Как сделать так, чтобы доходили до конца и шли в низ?
+
+
+/*
+int[,] CreateMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = (i * matrix.GetLength(1)) + j + 1;//печатаем массив с начала и далее прибавляем по 1 единице
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($" {matrix[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] NewMatrix = new int[4, 4];
+CreateMatrix(NewMatrix);
+PrintMatrix(NewMatrix);
+*/
+using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n;
+            Console.WriteLine("Введите размерность массива");
+            n = Convert.ToInt32(Console.ReadLine());
+            var a = GetSpire(n);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(a[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static int[,] GetSpire(int n)
+        {
+            var result = new int[n, n];
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    result[i, j] = rand.Next(n, n);
+            for (int currentChar = 1, padding = 0; padding < n / 2; padding++)
+            {
+                for (int j = padding; j < n - padding; j++)
+                    result[padding, j] = currentChar;
+                for (int j = padding; j < n - padding; j++)
+                    result[n - padding - 1, j] = currentChar;
+                for (int i = padding + 2; i < n - padding - 1; i++)
+                    result[i, padding] = currentChar;
+                for (int i = padding + 1; i < n - padding - 1; i++)
+                    result[i, n - padding - 1] = currentChar;
+                currentChar = 1 - currentChar;
+                result[padding + 1, padding] = currentChar;
+            }
+            if (n % 2 != 0 && result[0, 0] == 1)
+                result[n / 2, n / 2] = 1;
+            return result;
+        }
+    }
+}
+
+/*
+2. В двумерном массиве целых чисел. Удалить строку и столбец, на пересечении которых расположен наименьший элемент.
+*/
+
+
+
+/*
+3. Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы 
+соответствующего элемента
+*/
